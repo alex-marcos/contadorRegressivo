77eca158ed3e54358aa73fdcfdaaf7196aff7452
Laetuus <dwalexdw@gmail.com>
2022-08-08 00:17:57 -0300

    Final da estrutura de JavaScript e o link entre o JavaScript e o HTML.

--- scripts.js
+++ scripts.js
@@ -1,32 +1,15 @@
+import countdown from "./countdown.js";
 
-function startTimer(duration, display) {
-    var timer = duration, days, hours, minutes, seconds;
+const tempoParaOLançamento = new countdown("21 August 2022 23:00:00 GMT-0300");
+const tempos = document.querySelectorAll("[data-time]");
+
+
+function mostrarTempo() {
+    tempos.forEach((tempo, index) => {
+        tempo.innerHTML = tempoParaOLançamento.total[index];
+    });
     
-    setInterval(function(){
-      days = parseInt(timer  * 60 * 60 * 1000)  
-      hours = parseInt(timer  * 60 * 1000)  
-      minutes = parseInt(timer /60, 1000);
-      seconds = parseInt(timer % 60, 1000);
+}
 
-      days = days < 10 ? "0" + days : days;
-      hours = hours < 10 ? "0" + hours : hours;
-      minutes = minutes < 10 ?  "0" + minutes : minutes;
-      seconds = seconds < 10 ?  "0" + seconds : seconds;
-
-      display.textContent = days + ":" + hours + ":" + minutes + ":" + seconds;
-      
-      if(--timer < 0){
-        timer = duration;
-
-      }
-
-    }, 1000);
-  }
-
-  window.onload = function() {
-    var duration = 60 *4; //Conversão para segundos
-    var display = document.querySelector("#timer"); //Elemento para exibir o timer
-
-    startTimer(duration, display); //Inicia a função
-
-  }
+mostrarTempo();
+setInterval(mostrarTempo, 1000);
